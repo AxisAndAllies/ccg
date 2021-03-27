a88c13ffa2eceba141bb3b465e97e8868f05289f
cktang88 <arctangent20@yahoo.com>
2021-03-27 18:03:24 -0500

    i

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { DragDropContext } from 'react-beautiful-dnd';
 import Board from './game/board'
 
 function App() {
--- src/game/board.jsx
+++ src/game/board.jsx
@@ -85,58 +85,7 @@ function Board() {
             >
               <h2>{column.name}</h2>
               <div style={{ margin: 8 }}>
-                <Droppable droppableId={columnId} key={columnId}>
-                  {(provided, snapshot) => {
-                    return (
-                      <div
-                        {...provided.droppableProps}
-                        ref={provided.innerRef}
-                        style={{
-                          background: snapshot.isDraggingOver
-                            ? 'lightblue'
-                            : 'lightgrey',
-                          padding: 4,
-                          width: 250,
-                          minHeight: 500,
-                        }}
-                      >
-                        {column.items.map((item, index) => {
-                          return (
-                            <Draggable
-                              key={item.id}
-                              draggableId={item.id}
-                              index={index}
-                            >
-                              {(provided, snapshot) => {
-                                return (
-                                  <div
-                                    ref={provided.innerRef}
-                                    {...provided.draggableProps}
-                                    {...provided.dragHandleProps}
-                                    style={{
-                                      userSelect: 'none',
-                                      padding: 16,
-                                      margin: '0 0 8px 0',
-                                      minHeight: '50px',
-                                      backgroundColor: snapshot.isDragging
-                                        ? '#263B4A'
-                                        : '#456C86',
-                                      color: 'white',
-                                      ...provided.draggableProps.style,
-                                    }}
-                                  >
-                                    {item.content}
-                                  </div>
-                                );
-                              }}
-                            </Draggable>
-                          );
-                        })}
-                        {provided.placeholder}
-                      </div>
-                    );
-                  }}
-                </Droppable>
+                <Column columnId={columnId} column={column} />
               </div>
             </div>
           );
@@ -145,5 +94,54 @@ function Board() {
     </div>
   );
 }
+
+const Column = ({ columnId, column }) => (
+  <Droppable droppableId={columnId} key={columnId}>
+    {(provided, snapshot) => {
+      return (
+        <div
+          {...provided.droppableProps}
+          ref={provided.innerRef}
+          style={{
+            background: snapshot.isDraggingOver ? 'lightblue' : 'lightgrey',
+            padding: 4,
+            width: 250,
+            minHeight: 500,
+          }}
+        >
+          {column.items.map((item, index) => {
+            return (
+              <Draggable key={item.id} draggableId={item.id} index={index}>
+                {(provided, snapshot) => {
+                  return (
+                    <div
+                      ref={provided.innerRef}
+                      {...provided.draggableProps}
+                      {...provided.dragHandleProps}
+                      style={{
+                        userSelect: 'none',
+                        padding: 16,
+                        margin: '0 0 8px 0',
+                        minHeight: '50px',
+                        backgroundColor: snapshot.isDragging
+                          ? '#263B4A'
+                          : '#456C86',
+                        color: 'white',
+                        ...provided.draggableProps.style,
+                      }}
+                    >
+                      {item.content}
+                    </div>
+                  );
+                }}
+              </Draggable>
+            );
+          })}
+          {provided.placeholder}
+        </div>
+      );
+    }}
+  </Droppable>
+);
 
 export default Board;
